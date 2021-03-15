8088f4b7121360257b567b5bed7cb67f617e1fdc
Frank Nolasco Velasquez <49081234+FranksitoKawai@users.noreply.github.com>
2021-03-14 20:16:01 -0500

    mejora

--- juego.js
+++ juego.js
@@ -1,154 +1,30 @@
 let si = document.getElementById("si");
 let no = document.getElementById("no");
-let no1 = document.getElementById("no1");
-let no2 = document.getElementById("no2");
-let no3 = document.getElementById("no3");
-let no4 = document.getElementById("no4");
-let no5 = document.getElementById("no5");
-let no6 = document.getElementById("no6");
-let no7 = document.getElementById("no7");
-let no8 = document.getElementById("no8");
-let no9 = document.getElementById("no9");
-let no10 = document.getElementById("no10");
-let no11 = document.getElementById("no11");
-let no12 = document.getElementById("no12");
-let no13 = document.getElementById("no13");
-let no14 = document.getElementById("no14");
-let no15 = document.getElementById("no15");
-let no16 = document.getElementById("no16");
-let no17 = document.getElementById("no17");
-let no18 = document.getElementById("no18");
-let no19 = document.getElementById("no19");
-let no20 = document.getElementById("no20");
-
 si.addEventListener("click", sii);
 function sii(){
     alert("sabia que ibas a decir que si jeje");
 }
 
 no.addEventListener("mouseover", function (){
-    no.style.display = "block"; //Puedes usar inline o cualquier otro
-    no.style.display = "none";
-    no1.style.display = "block"
-} )
+    cambiarPosicion()
+})
+no.addEventListener("onfocus", function (){
+  cambiarPosicion()
+})
 
-no1.addEventListener('mouseover', function() {
-    no1.style.display = "block";
-    no1.style.display = "none"; 
-    no2.style.display = "block"
-  });
+const cambiarPosicion = () => {
+  const newCoords = generarNuevasCoordenadas()
+  no.style.top = newCoords.newTop+"px";
+  no.style.left = newCoords.newLeft+"px";
+}
 
-no2.addEventListener('mouseover', function() {
-    no2.style.display = "block";
-    no2.style.display = "none"; 
-    no3.style.display = "block"
-  });
 
-no3.addEventListener('mouseover', function() {
-    no3.style.display = "block";
-    no3.style.display = "none"; 
-    no4.style.display = "block"
-  });
-
-no4.addEventListener('mouseover', function() {
-    no4.style.display = "block";
-    no4.style.display = "none"; 
-    no5.style.display = "block"
-  });
-
-no5.addEventListener('mouseover', function() {
-    no5.style.display = "block";
-    no5.style.display = "none"; 
-    no6.style.display = "block"
-  });
-
-no6.addEventListener('mouseover', function() {
-    no6.style.display = "block";
-    no6.style.display = "none"; 
-    no7.style.display = "block"
-  });
-
-no7.addEventListener('mouseover', function() {
-    no7.style.display = "block";
-    no7.style.display = "none"; 
-    no8.style.display = "block"
-  });
-
-no8.addEventListener('mouseover', function() {
-    no8.style.display = "block";
-    no8.style.display = "none"; 
-    no9.style.display = "block"
-  });
-
-no9.addEventListener('mouseover', function() {
-    no9.style.display = "block";
-    no9.style.display = "none"; 
-    no10.style.display = "block"
-  });
-
-no10.addEventListener('mouseover', function() {
-    no10.style.display = "block";
-    no10.style.display = "none"; 
-    no11.style.display = "block"
-  });
-
-no11.addEventListener('mouseover', function() {
-    no11.style.display = "block";
-    no11.style.display = "none"; 
-    no12.style.display = "block"
-  });
-
-no12.addEventListener('mouseover', function() {
-    no12.style.display = "block";
-    no12.style.display = "none"; 
-    no13.style.display = "block"
-  });
-
-no13.addEventListener('mouseover', function() {
-    no13.style.display = "block";
-    no13.style.display = "none"; 
-    no14.style.display = "block"
-  });
-
-no14.addEventListener('mouseover', function() {
-    no14.style.display = "block";
-    no14.style.display = "none"; 
-    no15.style.display = "block"
-  });
-
-no15.addEventListener('mouseover', function() {
-    no15.style.display = "block";
-    no15.style.display = "none"; 
-    no16.style.display = "block"
-  });
-
-no16.addEventListener('mouseover', function() {
-    no16.style.display = "block";
-    no16.style.display = "none"; 
-    no17.style.display = "block"
-  });
-
-no17.addEventListener('mouseover', function() {
-    no17.style.display = "block";
-    no17.style.display = "none"; 
-    no18.style.display = "block"
-  });
-
-no18.addEventListener('mouseover', function() {
-    no18.style.display = "block";
-    no18.style.display = "none"; 
-    no19.style.display = "block"
-
-  });
-  
-no19.addEventListener('mouseover', function() {
-    no19.style.display = "block";
-    no19.style.display = "none"; 
-    no20.style.display = "block"
-  });
-
-no20.addEventListener('mouseover', function() {
-    no20.style.display = "block";
-    no20.style.display = "none"; 
-    no20.style.display = "none"
-  });
+const generarNuevasCoordenadas = () => {
+  const newTop = Math.random()*300;
+  const newLeft = Math.random()*300;
+  if(newTop > 150 && newLeft > 150) {
+    return { newTop , newLeft }
+  }else{
+    return generarNuevasCoordenadas()
+  }
+}
